4b28b9e5e81755c8691491f2dd456a47069adc3c
Candra <liaocandra888@gmail.com>
2021-05-13 17:46:00 +0700

    SharedPreferences

--- app/src/main/java/com/example/percobaan/Aktivitas_1.kt
+++ app/src/main/java/com/example/percobaan/Aktivitas_1.kt
@@ -1,11 +1,21 @@
 package com.example.percobaan
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class Aktivitas_1 : AppCompatActivity() {
+    lateinit var sharedPreferences: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_aktivitas_1)
+
+        val preferences=getSharedPreferences("SHARED_PREF", Context.MODE_PRIVATE)
+        var User=preferences.getString("Teks1","")
+        var Judul:TextView=findViewById(R.id.TV_Judul)
+        Judul.text=User
     }
 }
--- app/src/main/java/com/example/percobaan/MainActivity.kt
+++ app/src/main/java/com/example/percobaan/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.percobaan
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -8,21 +10,39 @@ import android.widget.EditText
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    lateinit var sharedPreferences: SharedPreferences
+    var isRemember=false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val Login:Button=findViewById(R.id.BTN_Login)
-        val User:EditText=findViewById(R.id.ET_User_Name)
-        val Pass:EditText=findViewById(R.id.ET_Password)
+        sharedPreferences=getSharedPreferences("SHARED_PREF", Context.MODE_PRIVATE)
+        isRemember=sharedPreferences.getBoolean("CHECKBOX",false)
+
+        if(isRemember){
+            val intent=Intent(this,Aktivitas_1::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        var Login:Button=findViewById(R.id.btnLogin)
+        var User:EditText=findViewById(R.id.etUserName)
+        var Pass:EditText=findViewById(R.id.etPassWord)
 
         Login.setOnClickListener {
-            if(User.text.toString()=="admim"&&Pass.text.toString()=="12345"){
-                intent=Intent(this,Aktivitas_1::class.java)
+            if(User.text.toString()=="admin"&&Pass.text.toString()=="12345"){
+                val editor:SharedPreferences.Editor=sharedPreferences.edit()
+                editor.putString("Teks1",User.text.toString())
+                editor.putString("Teks2",Pass.text.toString())
+                editor.apply()
+
+                val intent=Intent(this,Aktivitas_1::class.java)
                 startActivity(intent)
+                finish()
             }
             else{
-                val toast=Toast.makeText(applicationContext,"User atau Password Salah",Toast.LENGTH_SHORT)
+                val toast=Toast.makeText(applicationContext,"User atau Password salah",Toast.LENGTH_SHORT)
                 toast.show()
             }
         }
