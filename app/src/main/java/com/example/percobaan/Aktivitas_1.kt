76382b3f09c2be4eb3c9deb78094295ec06cb0d1
Candra <liaocandra888@gmail.com>
2021-05-14 21:07:08 +0700

    Logout

--- app/src/main/java/com/example/percobaan/Aktivitas_1.kt
+++ app/src/main/java/com/example/percobaan/Aktivitas_1.kt
@@ -4,12 +4,24 @@ import android.content.Context
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import android.widget.Button
 
 class Aktivitas_1 : AppCompatActivity() {
+    lateinit var sharedPreferences: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_aktivitas_1)
+
+        sharedPreferences=getSharedPreferences("SHARED_PREF", Context.MODE_PRIVATE)
+
+        val Logout: Button =findViewById(R.id.btnLogout)
+
+        Logout.setOnClickListener {
+            val editor:SharedPreferences.Editor=sharedPreferences.edit()
+            editor.putBoolean("isLogin",false)
+            editor.apply()
+        }
 
     }
 }
